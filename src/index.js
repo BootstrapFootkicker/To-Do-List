2dc9e1306e1140ac7d96961d04a251a3f7d93bfb
BootstrapFootkicker <89753423+BootstrapFootkicker@users.noreply.github.com>
2023-03-21 23:22:07 -0600

    Dom Manipulator Added Dom Manipulator class Added addTaskFolderToDom function Added addTaskToDom function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
 import {compareAsc, format} from 'date-fns'
 import css from "./style.css";
 
+class  DomController{
+
+    addTaskFolderToDom(TaskFolder){
+        let taskContainerContent= document.querySelector(".tasks-container-content");
+        let taskFolder = document.createElement('a')
+        let taskFolderListContainer = document.createElement('li')
+        taskFolder.innerText=TaskFolder.getName();
+        taskFolderListContainer.appendChild(taskFolder)
+        taskContainerContent.appendChild(taskFolderListContainer);
+
+    }
+    addTaskToDom(Task){
+        let taskDetailContainer = document.querySelector('.task-detail-container');
+        let task = document.createElement('div');
+        let checkBoxContainer = document.createElement('div');
+        let checkBox  = document.createElement('input');
+        let taskInfo  = document.createElement('div');
+        let taskDate = document.createElement('div');
+
+        task.classList.add('task')
+        checkBoxContainer.classList.add('checkbox-container')
+        taskInfo.classList.add('task-info')
+        // taskDate.classList.add('')
+
+
+        checkBox.type='checkbox'
+
+        taskInfo.innerText=Task.getDescription();
+        taskDate.innerText=Task.getDueDate();
+
+
+        checkBoxContainer.appendChild(checkBox);
+        checkBoxContainer.appendChild(taskInfo);
+        task.appendChild(checkBoxContainer);
+        task.appendChild(taskDate);
+
+        taskDetailContainer.appendChild(task);
+    }
+
+    //add way to find specific dom element and remove task. probably take id from check box or add id to task
+}
 
 class Task {
     constructor(name, description, dueDate) {
@@ -27,10 +68,15 @@ class Task {
 
 class TaskFolder{
     constructor(name) {
+        this.name=name;
         this._taskList=[]
     }
 
     //Getters
+    getName(){
+        return this.name;
+    }
+
     getTaskList(){
         return this._taskList;
     }
@@ -45,4 +91,22 @@ class TaskFolder{
         //find index of task using dom Id and pop task maybe?
     }
 }
+
+
+
+let testFolder = new  TaskFolder('this is a test');
+let domTester= new DomController();
+let doit=new Task('that','test','02/04/66')
+
+
+domTester.addTaskFolderToDom(testFolder);
+domTester.addTaskFolderToDom(testFolder);
+domTester.addTaskFolderToDom(testFolder);
+domTester.addTaskFolderToDom(testFolder);
+
+domTester.addTaskToDom(doit)
+domTester.addTaskToDom(doit)
+domTester.addTaskToDom(doit)
+domTester.addTaskToDom(doit)
+domTester.addTaskToDom(doit)
 
